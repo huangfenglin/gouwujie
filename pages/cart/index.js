85ace039d7286c56416cd59a6e0064ad3c1913b7
huangfenglin <effort0755@163.com>
2019-12-03 20:41:21 +0800

    购物车实现商品选中

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -7,7 +7,10 @@ Page({
    */
   data: {
     // 用户的收货地址
-    address: {}
+    address: {},
+    carts: [],
+    // 商品的总价格
+    totalPrice:0
   },
 
   /**
@@ -23,11 +26,14 @@ Page({
   onShow: function () {
     // adress = {对象} || 空字符串
     const address = wx.getStorageSync("address" || {});
+    const carts = wx.getStorageSync("carts") || []
     this.setData({
-      address
+      address,
+      carts
     })
-      
+      this.countAll(carts)
   },
+  // 获取通讯地址的一坨方法
   async handleFinalGet() {
     // 1 先获取用户授权状态
     const auth = (await getSetting()).authSetting["scope.address"];
@@ -37,5 +43,37 @@ Page({
     const res = await chooseAddress(); 
      // 把数据存到缓存中
      wx.setStorageSync("address", res);
-  }
+  },
+  // 计算总价格 -  计算购买的数量
+  countAll(carts) {
+    let totalPrice = 0;
+    let nums = 0;
+    carts.forEach(v=>{
+      if(v.isChecked){
+        totalPrice += v.nums*v.goods_price
+        // 我们要的是总的数量 而不是要购买的种类！！
+        nums += v.nums
+      }
+      this.setData({
+        totalPrice,
+        nums
+      })
+    })
+
+  },
+    // 单个商品的选中事件
+    checkboxChange(e) {
+      // 根据索引拿到当前商品
+      const {index} = e.currentTarget.dataset;
+      let {carts} = this.data
+       // 对选中状态 做取反
+       carts[index].isChecked=!carts[index].isChecked;
+       // 2 把数组 设置回 data中 和 缓存中
+       this.setData({
+         carts
+       })
+       wx.setStorageSync("carts", carts); 
+       // 重新计算总价格   小程序中没有计算属性（页面中没有计算属性，组件中却是有计算属性！！！）
+       this.countAll(carts);
+    }
 })
